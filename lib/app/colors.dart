cea2011593156a3b086aa04d5bb1ef7a902b6d7f
UcGeorge <petegeorge20005@gmail.com>
2023-03-21 23:07:02 +0100

    initial commit

---
+++ lib/app/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color accentGreen = const Color(0xff10DAB4);
+  static Color orange = Colors.orange;
+  static Color primaryBlue = const Color(0xff1E284A);
+  static Color red = Colors.red;
+  static Color white = Colors.white;
+  static Color textFieldWhite = Colors.white.withOpacity(.7);
+  static Color textFieldBackground = const Color(0xff151113);
+  static Color textFieldHintText = const Color(0xff595A65);
+}
